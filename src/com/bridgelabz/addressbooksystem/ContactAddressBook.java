33da58f823d38b7b0d4f893f0ecb8fa7f2d860fc
rajshetye11 <rajshetye11@gmail.com>
2021-08-17 01:49:05 +0530

    [raj] : added create contact in Address Book

---
+++ src/com/bridgelabz/addressbooksystem/ContactAddressBook.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.addressbooksystem;
+
+public class ContactAddressBook {
+	
+	String firstname;
+	String lastname;
+	String address;
+	String city;
+	String state;
+	int zipcode;
+	int phonenum;
+	String email;
+	
+	ContactAddressBook(String firstname, String lastname, String address, 
+						String city, String state, int zipcode, int phonenum, 
+						String email){
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.address = address; 
+		this.city = city; 
+		this.state = state;
+		this.zipcode = zipcode;
+		this.phonenum=phonenum; 
+		this.email=email;
+		
+	}
+
+	@Override
+	public String toString() {
+		return "ContactAddressBook [firstname=" + firstname + ", lastname=" + lastname + ", address=" + address
+				+ ", city=" + city + ", state=" + state + ", zipcode=" + zipcode + ", phonenum=" + phonenum + ", email="
+				+ email + "]";
+	}
+	
+	
+}
